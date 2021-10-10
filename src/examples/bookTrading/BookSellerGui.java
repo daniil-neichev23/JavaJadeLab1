f38b4519285185e6812916921dadd1ee345afad1
Daniil <56074212+daniil-neichev23@users.noreply.github.com>
2021-10-10 12:01:04 +0300

    Update BookSellerGui.java

--- src/examples/bookTrading/BookSellerGui.java
+++ src/examples/bookTrading/BookSellerGui.java
@@ -1,20 +1,3 @@
-/*****************************************************************
-JADE - Java Agent DEvelopment Framework is a framework to develop 
-multi-agent systems in compliance with the FIPA specifications.
-Copyright (C) 2000 CSELT S.p.A. 
-
-GNU Lesser General Public License
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation, 
-version 2.1 of the License. 
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
 
 package examples.bookTrading;
 
